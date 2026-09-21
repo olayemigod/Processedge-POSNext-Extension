51b11aabf4f53e03a8a215297394df8a35de7bea
olayemigod <olayemigod@gmail.com>
2026-09-21 19:34:43 +0100

    reapply POSNext permission compatibility: processedge_posnext_override/install.py

--- processedge_posnext_override/install.py
+++ processedge_posnext_override/install.py
@@ -1,9 +1,12 @@
 from processedge_posnext_override.overrides.pos_settings import ensure_posnext_settings_sync
+from processedge_posnext_override.permissions import ensure_posnext_permission_compatibility
 
 
 def after_install():
     ensure_posnext_settings_sync()
+    ensure_posnext_permission_compatibility()
 
 
 def after_migrate():
     ensure_posnext_settings_sync()
+    ensure_posnext_permission_compatibility()
